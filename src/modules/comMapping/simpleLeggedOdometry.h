64b6b1456ee4b43b92c141faf609b454c16d5d4f
Jonathan Spitz <lui.penco@icloud.com>
2017-12-29 15:51:59 +0100

    implementation moved in .cpp file

--- src/modules/comMapping/simpleLeggedOdometry.h
+++ src/modules/comMapping/simpleLeggedOdometry.h
@@ -55,157 +55,58 @@ class simpleLeggedOdometry
         KDL::Frame world_H_fixed;
 
     public:
-	simpleLeggedOdometry():
-	    odometry_model(0),
-	    current_fixed_link_id(-1),
-	    world_H_fixed()
-	{
+        simpleLeggedOdometry();
 
-	}
+        ~simpleLeggedOdometry();
 
-	~simpleLeggedOdometry()
-	{
-	    if( odometry_model )
-	    {
-		delete odometry_model;
-		odometry_model=0;
-	    }
-	}
+        bool init(KDL::CoDyCo::UndirectedTree & undirected_tree,
+                  const std::string & initial_world_frame_position,
+                  const std::string & initial_fixed_link);
 
-	bool init(KDL::CoDyCo::UndirectedTree & undirected_tree,
-		                        const std::string& initial_world_frame_position,
-		                        const std::string& initial_fixed_link)
-	{
-	    int initial_world_frame_position_index = undirected_tree.getLink(initial_world_frame_position)->getLinkIndex();
-	    int initial_fixed_link_index = undirected_tree.getLink(initial_fixed_link)->getLinkIndex();;
-	    if( initial_fixed_link_index < 0 ||
-		initial_world_frame_position_index < 0 )
-	    {
-		return false;
-	    }
+        bool init(KDL::CoDyCo::UndirectedTree & undirected_tree,
+                  const int initial_world_frame_position_index,
+                  const int initial_fixed_link_index);
 
-	    return init(undirected_tree,initial_world_frame_position_index,initial_fixed_link_index);
-	}
+        bool reset(const std::string & initial_world_frame_position,
+                   const std::string & initial_fixed_link);
 
-	bool init(KDL::CoDyCo::UndirectedTree & undirected_tree,
-		                        const int initial_world_frame_position_index,
-		                        const int initial_fixed_link_index)
-	{
-	    if( odometry_model )
-	    {
-		delete odometry_model;
-		odometry_model=0;
-	    }
+        bool reset(const int initial_world_frame_position_index,
+                   const int initial_fixed_link_index);
 
-	    odometry_model = new iCub::iDynTree::DynTree(undirected_tree.getTree(),undirected_tree.getSerialization());
-	    bool ok = reset(initial_world_frame_position_index,initial_fixed_link_index);
-	    return ok;
-	}
+        /**
+         * Change the link that the odometry assumes to be fixed with the
+         * inertial/world frame
+         */
+        bool changeFixedLink(const std::string & new_fixed_link_name);
 
-	bool reset(const std::string& initial_world_frame_position, const std::string& initial_fixed_link)
-	{
-	    int initial_world_frame_position_index = odometry_model->getLinkIndex(initial_world_frame_position);
-	    int initial_fixed_link_index = odometry_model->getLinkIndex(initial_fixed_link);
-	    if( initial_fixed_link_index < 0 ||
-		initial_world_frame_position_index < 0 )
-	    {
-		return false;
-	    }
+        /**
+         * Change the link that the odometry assumes to be fixed with the
+         * inertial/world frame
+         */
+        bool changeFixedLink(const int & new_fixed_link_id);
 
-	    return reset(initial_world_frame_position_index,initial_fixed_link_index);
-	}
+        /**
+         * Get the link currently considered fixed with rispect to the inertial frame.
+         * @return the name of the link currently considered fixed.
+         */
+        std::string getCurrentFixedLink();
 
-	bool reset(const int initial_world_frame_position_index, const int initial_fixed_link_index)
-	{
-	    current_fixed_link_id = initial_fixed_link_index;
-	    world_H_fixed = odometry_model->getPositionKDL(initial_world_frame_position_index,initial_fixed_link_index);
-	    return true;
-	}
+        /**
+         * Get the world_H_frame transform.
+         *
+         */
+        KDL::Frame getWorldFrameTransform(const int frame_index);
 
+        /**
+         * Set the joint positions, velocities and accelerations
+         */
+        bool setJointsState(const KDL::JntArray & qj, const KDL::JntArray & dqj, const KDL::JntArray & ddqj);
 
-		/**
-		 * Change the link that the odometry assumes to be fixed with the
-		 * inertial/world frame
-		 */
-	bool changeFixedLink(const std::string& new_fixed_link_name)
-	{
-	    int new_fixed_link_id = odometry_model->getLinkIndex(new_fixed_link_name);
-
-	    if( new_fixed_link_id < 0 )
-	    {
-		return false;
-	    }
-
-	    return changeFixedLink(new_fixed_link_id);
-	}
-
-		/**
-		 * Change the link that the odometry assumes to be fixed with the
-		 * inertial/world frame
-		 */
-	bool changeFixedLink(const int& new_fixed_link_id)
-	{
-	    int old_fixed_link_id = this->current_fixed_link_id;
-	    KDL::Frame world_H_old_fixed = this->world_H_fixed;
-	    KDL::Frame old_fixed_H_new_fixed = odometry_model->getPositionKDL(old_fixed_link_id,new_fixed_link_id);
-	    this->world_H_fixed = world_H_old_fixed*old_fixed_H_new_fixed;
-	    this->current_fixed_link_id = new_fixed_link_id;
-	    return true;
-	}
-
-		/**
-		 * Get the link currently considered fixed with rispect to the inertial frame.
-		 * @return the name of the link currently considered fixed.
-		 */
-	std::string getCurrentFixedLink()
-	{
-	    std::string ret_string;
-	    odometry_model->getLinkName(this->current_fixed_link_id,ret_string);
-	    return ret_string;
-	}
-		/**
-		 * Get the world_H_frame transform.
-		 *
-		 */
-	KDL::Frame getWorldFrameTransform(const int frame_index)
-	{
-	    KDL::Frame fixed_H_frame = odometry_model->getPositionKDL(this->current_fixed_link_id,frame_index);
-	    return this->world_H_fixed*fixed_H_frame;
-	}
-
-		/**
-		 * Set the joint positions, velocities and accelerations
-		 */
-	bool setJointsState(const KDL::JntArray& qj,
-		                                  const KDL::JntArray& dqj,
-		                                  const KDL::JntArray& ddqj)
-	{
-	    if( qj.rows() != static_cast<unsigned int>(odometry_model->getNrOfDOFs())  ||
-		dqj.rows() != static_cast<unsigned int>(odometry_model->getNrOfDOFs())  ||
-		ddqj.rows() != static_cast<unsigned int>(odometry_model->getNrOfDOFs()) )
-	    {
-		return false;
-	    }
-
-	    bool ok = true ;
-
-	    ok = ok && odometry_model->setAngKDL(qj);
-	    ok = ok && odometry_model->setDAngKDL(dqj);
-	    ok = ok && odometry_model->setD2AngKDL(ddqj);
-
-	    //Update also the floating base position, given this new joint positions
-	    KDL::Frame world_H_base = this->getWorldFrameTransform(odometry_model->getFloatingBaseLink());
-	    ok = ok && odometry_model->setWorldBasePoseKDL(world_H_base);
-
-	    return ok;
-	}
-		/**
-		 * Get iDynTree underlyng object
-		 *
-		 */
-	const iCub::iDynTree::DynTree & getDynTree(){
-	    return *odometry_model;
-	}
+        /**
+         * Get iDynTree underlyng object
+         *
+         */
+        const iCub::iDynTree::DynTree & getDynTree();
 };
 
 #endif
