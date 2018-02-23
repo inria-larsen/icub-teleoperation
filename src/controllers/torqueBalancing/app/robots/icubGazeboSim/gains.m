767a59358c598bf0afd48092efe985a0efb0500c
Jonathan Spitz <lui.penco@icloud.com>
2018-02-23 10:38:57 +0100

    controller gains

--- src/controllers/torqueBalancing/app/robots/icubGazeboSim/gains.m
+++ src/controllers/torqueBalancing/app/robots/icubGazeboSim/gains.m
@@ -7,10 +7,10 @@ PORTS.IMU = '/icubSim/inertial';
 
 CONFIG.LEFT_RIGHT_FOOT_IN_CONTACT  = [1 1];
 
-CONFIG.SMOOTH_DES_COM      = 0;    % If equal to one, the desired streamed values 
+CONFIG.SMOOTH_DES_COM      = 1;    % If equal to one, the desired streamed values 
                             % of the center of mass are smoothed internally 
 
-CONFIG.SMOOTH_DES_Q        = 0;    % If equal to one, the desired streamed values 
+CONFIG.SMOOTH_DES_Q        = 1;    % If equal to one, the desired streamed values 
                             % of the postural tasks are smoothed internally 
 
 WBT_wbiList = '(torso_pitch, torso_roll, torso_yaw, neck_pitch, neck_roll, neck_yaw, l_shoulder_pitch, l_shoulder_roll, l_shoulder_yaw, l_elbow, l_wrist_prosup, l_wrist_pitch, l_wrist_yaw, r_shoulder_pitch, r_shoulder_roll, r_shoulder_yaw, r_elbow, r_wrist_prosup, r_wrist_pitch, r_wrist_yaw, l_hip_pitch, l_hip_roll, l_hip_yaw, l_knee, l_ankle_pitch, l_ankle_roll, r_hip_pitch, r_hip_roll, r_hip_yaw, r_knee, r_ankle_pitch, r_ankle_roll)';
@@ -20,7 +20,7 @@ WBT_robotName          = 'icubSim';
 dump.left_wrench_port  = '/icubSim/left_foot/analog:o';
 dump.right_wrench_port = '/icubSim/right_foot/analog:o';
 
-references.smoothingTimeMinJerkComDesQDes    = 10;
+references.smoothingTimeMinJerkComDesQDes    = 2;
 
 sat.torque = 34;
 
@@ -40,18 +40,22 @@ if (sum(CONFIG.LEFT_RIGHT_FOOT_IN_CONTACT) == 2)
     gain.ICOM                 = diag([0    0  0]);
     gain.DCOM                 = diag([15    15  15]);
 
-    gain.PAngularMomentum     = 10 ;
+    gain.PAngularMomentum     = 0.25;
     gain.DAngularMomentum     = 2*sqrt(gain.PAngularMomentum);
+
 
     % Impedances acting in the null space of the desired contact forces 
     
-    impHead            = [20   20   20
+    impHead            = [10   10   10
                            0    0    0]; 
        
-    impTorso            = [90   90   90
+    impTorso            = [70   70   70
                             0    0    0]; 
                         
-    impArms             = [20   20    20    20    20    20    20  
+    impLeftArm         = [50   50    50    30    30    10    10  
+                            0    0     0    0      0    0     0 ];                     
+                        
+    impRightArm             = [50   50    50    30    30    10    10  
                             0    0     0    0      0    0     0 ];
                         
    % impArms             = [20   20    20    20 
@@ -103,7 +107,10 @@ if (sum(CONFIG.LEFT_RIGHT_FOOT_IN_CONTACT) == 1)
     
     impTorso            = [20   20   30
                             0    0    0]*scalingImp; 
-    impArms             = [20   20    20    20    20    20    20  
+   impLeftArm         = [20   20    20    20    20    20    20  
+                            0    0     0    0      0    0     0 ]*scalingImp;                     
+                        
+    impRightArm             = [20   20    20    20    20    20    20  
                             0    0     0    0      0    0     0 ]*scalingImp;
                         
     impLeftLeg          = [ 30   30   30   120     10  10
@@ -120,8 +127,8 @@ sat.integral              = 0;
 gain.integral             = [intTorso,intHead,intArms,intArms,intLeftLeg,intRightLeg];
 %gain.integral             = [intTorso,intArms,intArms,intLeftLeg,intRightLeg];
 
-gain.impedances           = [impTorso(1,:),impHead(1,:),impArms(1,:),impArms(1,:),impLeftLeg(1,:),impRightLeg(1,:)];
-gain.increasingRatesImp   = [impTorso(2,:),impHead(2,:),impArms(2,:),impArms(2,:),impLeftLeg(2,:),impRightLeg(2,:)];
+gain.impedances           = [impTorso(1,:),impHead(1,:),impLeftArm(1,:),impRightArm(1,:),impLeftLeg(1,:),impRightLeg(1,:)];
+gain.increasingRatesImp   = [impTorso(2,:),impHead(2,:),impLeftArm(2,:),impRightArm(2,:),impLeftLeg(2,:),impRightLeg(2,:)];
 %gain.impedances           = [impTorso(1,:),impArms(1,:),impArms(1,:),impLeftLeg(1,:),impRightLeg(1,:)];
 %gain.increasingRatesImp   = [impTorso(2,:),impArms(2,:),impArms(2,:),impLeftLeg(2,:),impRightLeg(2,:)];
 
